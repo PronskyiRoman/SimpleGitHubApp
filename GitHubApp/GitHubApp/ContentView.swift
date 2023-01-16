62fadbe7d0474892397de3433f71fd2b7ac610b2
Roman-Pronskyi <92256917+PronskyiRoman@users.noreply.github.com>
2023-01-16 15:27:09 +0200

    initial project

---
+++ GitHubApp/GitHubApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHubApp
+//
+//  Created by -_- on 16.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubApp/GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by -_- on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
